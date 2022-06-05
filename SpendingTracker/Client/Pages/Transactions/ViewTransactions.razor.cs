774ab16b6553eaf35640b20b5fc1abfa85104ad9
Hannah Price <hannahprice94@hotmail.co.uk>
2022-06-05 14:21:58 +0100

    Created transactions page

---
+++ SpendingTracker/Client/Pages/Transactions/ViewTransactions.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using SpendingTracker.Client.Services;
+using SpendingTracker.Shared.Models;
+
+namespace SpendingTracker.Client.Pages.Transactions
+{
+    public partial class ViewTransactions
+    {
+
+        [Inject]
+        public ITransactionService TransactionService { get; set; }
+        public List<Transaction> Transactions { get; set; } = new List<Transaction>(0);
+        public bool IsLoading { get; set; } = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            IsLoading = true;
+            
+            Transactions = await TransactionService.GetAllTransactions();
+            
+            IsLoading = false;
+        }
+    }
+}
