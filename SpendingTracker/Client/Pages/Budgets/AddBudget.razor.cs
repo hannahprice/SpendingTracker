3e5e5f1ec3ecdf7fa66a0f4aa023e6c607663b1c
Hannah Price <hannahprice94@hotmail.co.uk>
2022-06-15 22:54:05 +0100

    WIP: Started add budget page. Layout tbd

---
+++ SpendingTracker/Client/Pages/Budgets/AddBudget.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SpendingTracker.Client.Services;
+using SpendingTracker.Shared.Models;
+
+namespace SpendingTracker.Client.Pages.Budgets
+{
+    public partial class AddBudget
+    {
+        [Inject]
+        public IBudgetsService BudgetsService { get; set; }
+        [Inject]
+        public ICategoriesService CategoriesService { get; set; }
+        public Budget Budget { get; set; } = new Budget();
+        public List<Category> AvailableCategories { get; set; } = new List<Category>();
+        public List<Subcategory> AvailableSubcategories { get; set; } = new List<Subcategory>();
+        MudChip[]? SelectedCategories { get; set; }
+        MudChip[]? SelectedSubcategories { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            AvailableCategories = await CategoriesService.GetAllCategories();
+            AvailableSubcategories = AvailableCategories.SelectMany(x => x.Subcategories).ToList();
+        }
+    }
+}
