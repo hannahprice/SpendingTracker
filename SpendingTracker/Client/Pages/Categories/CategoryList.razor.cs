a5c127d632a332520c5db102e6d23d8836f94738
Hannah Price <hannahprice94@hotmail.co.uk>
2022-07-25 07:56:30 +0100

    Changed categories list to be displayed in grouped table

--- SpendingTracker/Client/Pages/Categories/CategoryList.razor.cs
+++ SpendingTracker/Client/Pages/Categories/CategoryList.razor.cs
@@ -14,13 +14,29 @@ namespace SpendingTracker.Client.Pages.Categories
         public bool IsLoading { get; set; } = false;
         private List<Category> Categories { get; set; } = new List<Category>();
 
+        private TableGroupDefinition<Subcategory> _groupDefinition = new()
+        {
+            Indentation = false,
+            Expandable = true,
+            IsInitiallyExpanded = false,
+            Selector = (e) => e.CategoryId
+        };
+        
         protected override async Task OnInitializedAsync()
         {
             IsLoading = true;
 
             Categories = await CategoriesService.GetAllCategories();
-
+            Subcategories = Categories.Where(x => x.Subcategories != null && x.Subcategories.Any())
+                .SelectMany(x => x.Subcategories!).ToList();
+            
             IsLoading = false;
+        }
+
+        private string GetGroupName(object categoryId)
+        {
+            var id = int.Parse(categoryId.ToString());
+            return Categories.First(x => x.Id == id).Description;
         }
     }
 }
