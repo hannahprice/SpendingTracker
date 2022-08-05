921b4c3ab80a33f72a2499dd456eb271f72341d5
Hannah <hannahprice94@hotmail.co.uk>
2022-08-05 07:02:55 +0100

    Moved store initialise to app load from index page

---
+++ SpendingTracker/Client/App.razor.cs
@@ -0,0 +1,18 @@
+﻿using Fluxor;
+using Microsoft.AspNetCore.Components;
+using SpendingTracker.Client.Store.Budgets.Actions;
+using SpendingTracker.Client.Store.Transactions.Actions;
+
+namespace SpendingTracker.Client;
+
+public partial class App
+{
+    [Inject] private IDispatcher Dispatcher { get; set; }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        Dispatcher.Dispatch(new LoadBudgetsAction());
+        Dispatcher.Dispatch(new LoadTransactionsAction());
+    }
+}
--- SpendingTracker/Client/Pages/Index.razor.cs
+++ SpendingTracker/Client/Pages/Index.razor.cs
@@ -7,12 +7,12 @@ namespace SpendingTracker.Client.Pages;
 
 public partial class Index
 {
-    [Inject] private IDispatcher Dispatcher { get; set; }
-
-    protected override void OnInitialized()
-    {
-        base.OnInitialized();
-        Dispatcher.Dispatch(new LoadBudgetsAction());
-        Dispatcher.Dispatch(new LoadTransactionsAction());
-    }
+    // [Inject] private IDispatcher Dispatcher { get; set; }
+    //
+    // protected override void OnInitialized()
+    // {
+    //     base.OnInitialized();
+    //     Dispatcher.Dispatch(new LoadBudgetsAction());
+    //     Dispatcher.Dispatch(new LoadTransactionsAction());
+    // }
 }
