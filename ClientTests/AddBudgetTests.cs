c649fd3b8046432acb6580a6a26a73343e19b0a3
Hannah <hannahprice94@hotmail.co.uk>
2022-08-07 22:39:48 +0100

    Added ClientTests project and sample BUnit test

---
+++ ClientTests/AddBudgetTests.cs
@@ -0,0 +1,49 @@
+﻿using Bunit;
+using FluentAssertions;
+using Fluxor;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using MudBlazor;
+using MudBlazor.Services;
+using SpendingTracker.Client.Components;
+using SpendingTracker.Client.Pages.Budgets;
+using SpendingTracker.Client.Store.Budgets;
+using SpendingTracker.Shared.Models;
+
+namespace ClientTests;
+
+public class AddBudgetTests : TestContext
+{
+    private Mock<IState<BudgetsState>> BudgetsState;
+    private Mock<IDispatcher> Dispatcher;
+    private Mock<IActionSubscriber> ActionSubscriber;
+    
+    public AddBudgetTests()
+    {
+        BudgetsState = new Mock<IState<BudgetsState>>();
+        Dispatcher = new Mock<IDispatcher>();
+        ActionSubscriber = new Mock<IActionSubscriber>();
+        
+        BudgetsState.Setup(x => x.Value).Returns(new BudgetsState(isLoading: false, budgets: new List<Budget>(),
+            budgetDetail: null, multiAddEnabled: false));
+        
+        Services.AddSingleton(BudgetsState.Object);
+        Services.AddSingleton(Dispatcher.Object);
+        Services.AddSingleton(ActionSubscriber.Object);
+
+        ComponentFactories.AddStub<MudNumericField<decimal>>();
+        ComponentFactories.AddStub<MudSelect<Frequency>>();
+        ComponentFactories.AddStub<CategorySelection>();
+    }
+    
+    [Fact]
+    public void AddBudgetSampleTest()
+    {
+        // Act
+        var component = RenderComponent<AddBudget>();
+        
+        // Assert
+        var header = component.Find("#AddBudget-Header");
+        header.TextContent.Should().Be("Add new budget");
+    }
+}
--- SpendingTracker/Client/Components/CategorySelection.razor.cs
+++ SpendingTracker/Client/Components/CategorySelection.razor.cs
@@ -7,29 +7,16 @@ namespace SpendingTracker.Client.Components;
 
 public partial class CategorySelection
 {
-    public bool IsLoading { get; set; } = false;
-    public List<Category> AvailableCategories { get; set; } = new List<Category>();
-    public List<Subcategory> AvailableSubcategories { get; set; } = new List<Subcategory>();
-    public MudChip[]? SelectedCategoryChips { get; set; } 
-    public MudChip[]? SelectedSubcategoryChips { get; set; }
-    
-    [Parameter]
-    public List<Category> SelectedCategories { get; set; }
-    
-    [Parameter]
-    public EventCallback<List<Category>> SelectedCategoriesChanged { get; set; }
-    
-    [Parameter]
-    public List<Subcategory> SelectedSubcategories { get; set; }
-    
-    [Parameter]
-    public EventCallback<List<Subcategory>> SelectedSubcategoriesChanged { get; set; }
-
-    [Inject]
-    public ICategoriesService CategoriesService { get; set; }
-
-    public string Test { get; set; } = null;
-    
+    [Parameter] public List<Category> SelectedCategories { get; set; }
+    [Parameter] public EventCallback<List<Category>> SelectedCategoriesChanged { get; set; }
+    [Parameter] public List<Subcategory> SelectedSubcategories { get; set; }
+    [Parameter] public EventCallback<List<Subcategory>> SelectedSubcategoriesChanged { get; set; }
+    [Inject] public ICategoriesService CategoriesService { get; set; }
+    private bool IsLoading { get; set; } = false;
+    private List<Category> AvailableCategories { get; set; } = new List<Category>();
+    private List<Subcategory> AvailableSubcategories { get; set; } = new List<Subcategory>();
+    private MudChip[]? SelectedCategoryChips { get; set; } 
+    private MudChip[]? SelectedSubcategoryChips { get; set; }
     protected override async Task OnInitializedAsync()
     {
         IsLoading = true;
@@ -57,7 +44,7 @@ public partial class CategorySelection
         await SelectedSubcategoriesChanged.InvokeAsync(SelectedSubcategories);
     }
 
-    public List<Category> GetSelectedCategories()
+    private List<Category> GetSelectedCategories()
     {
         var selectedCategoryDescriptions = SelectedCategoryChips?.Select(c => c.Text).ToList();
         return AvailableCategories.Where(x => selectedCategoryDescriptions.Contains(x.Description)).ToList();
@@ -68,8 +55,6 @@ public partial class CategorySelection
         if (SelectedSubcategoryChips != null && SelectedSubcategoryChips.Length > 0)
         {
             var selectedSubcategoryDescriptions = SelectedSubcategoryChips?.Select(c => c.Text).ToList();
-            Test = string.Join(',', selectedSubcategoryDescriptions);
-
             return AvailableSubcategories.Where(x => selectedSubcategoryDescriptions.Contains(x.Description)).ToList();   
         }
         return new List<Subcategory>(0);
